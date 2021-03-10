dda4284964c4206ee3bd2334a6fb5189fef13767
Your Name <andrey.mglinec@gmail.com>
2021-03-10 17:58:10 +0300

    init:added solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0){
+    return 0;
+    }else {
+     let min=array[0];
+     for (let i=0; i<array.length;i++){
+       if (array[i]<min){
+       min=array[i];
+       }
+       }
+       return min; 
+   }
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0){
+    return 0;
+    }else {
+     let max=array[0];
+     for (let i=0; i<array.length;i++){
+       if (array[i]>max){
+       max=array[i];
+       }
+       }
+       return max; 
+   }
 }
-
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0){
+    return 0;
+    }else {
+     let sum=0;
+     for (let i=0; i<array.length;i++){
+        sum+=array[i];
+       }
+      let avg = sum/array.length;
+       return avg; 
+   }
 }
